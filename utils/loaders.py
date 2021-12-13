05f26b05ad2d2f5e67f3431304a9bb6c63d9d2b8
Jose Ricardo Mendoza Villar <richi_1325@hotmail.com>
2021-12-12 20:37:39 -0600

    project code

--- utils/loaders.py
+++ utils/loaders.py
@@ -250,59 +250,38 @@ def load_celeb(data_name, image_size, batch_size):
 
     return x_train
 
-from functools import partial
+from functools import partial, reduce
 
-def load_music(data_name, filename, n_bars, n_steps_per_bar):
+def load_music(data_name, filename, n_bars, n_steps_per_bar,max_number):
     file = os.path.join("data", data_name, filename)
 
     with np.load(file, encoding='bytes',allow_pickle=True) as f:
-        print(f)
-        data = f['train']
+        #print(f)
+        data_ints = f['train']
     
-    data_ints = []
-
-    for x in data:
-        counter = 0
-        cont = True
-        while cont:
-            if not np.any(np.isnan(x[counter:(counter+4)])):
-                cont = False
-            else:
-                counter += 4
-
-        if n_bars * n_steps_per_bar < x.shape[0]:
-            data_ints.append(x[counter:(counter + (n_bars * n_steps_per_bar)),:])
-
-
-    data_ints = np.array(data_ints)
-
-    n_songs = data_ints.shape[0]
+    #print(data_ints.shape)
+    n_songs = data_ints.shape[0]*4
     n_tracks = data_ints.shape[2]
 
     data_ints = data_ints.reshape([n_songs, n_bars, n_steps_per_bar, n_tracks])
-
-    max_note = 83
-
+    data_inst = np.abs(data_ints-217)
+    #print(data_ints.shape)
+    max_note = max_number
     where_are_NaNs = np.isnan(data_ints)
     data_ints[where_are_NaNs] = max_note + 1
     max_note = max_note + 1
-
     data_ints = data_ints.astype(int)
-
     num_classes = max_note + 1
-
-    
     data_binary = np.eye(num_classes)[data_ints]
     data_binary[data_binary==0] = -1
     data_binary = np.delete(data_binary, max_note,-1)
-
-    data_binary = data_binary.transpose([0,1,2, 4,3])
-    
-    
-
-    
-
-    return data_binary, data_ints, data
+    shape_pro = reduce(lambda a, b: a*b, data_binary.shape)
+    shape_songs = shape_pro//2//16//84//4*2*4*16*84
+    data_binary = data_binary.reshape(shape_pro,1)
+    data_binary = data_binary[:shape_songs]                  
+    data_binary = data_binary.reshape(shape_songs//2//16//84//4,2, 16, 84, 4)    
+    #data_binary = data_binary.transpose([0,1,2, 4,3])
+    return data_binary, data_ints
 
 
 def preprocess_image(data_name, file, img_nrows, img_ncols):
